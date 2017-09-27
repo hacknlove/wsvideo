beaf42a98dcf7465bc7cb5c13ffc01ffdbdb1fc6
antonio <antonio@pykiss.com>
2017-09-27 13:27:42 +0200

    estable

--- source.js
+++ source.js
@@ -12,45 +12,83 @@ var WsFile = function (path) {
 }
 
 WsFile.prototype.createReadStream = function (opts) {
-  var self = this
-  var socket
   opts = opts || {}
   opts.start = opts.start || 0
+  var self = this
+  var socket = new WebSocket(self.path + '?start=' + opts.start)
+  self.socket = socket
   var rs = new EventEmitter()
-  rs.pipe = function (writable) {
-    socket = new WebSocket(self.path + '?start=' + opts.start)
-    socket.addEventListener('open', function () {
-      socket.addEventListener('message', function (data) {
-        blobToBuffer(data.data, function (err, buffer) {
-          if (err) return console.log(err)
-          try {
-            var drain = writable.write(buffer)
-          } catch (e) {
-            console.log(e)
-          }
-          if (drain === false) {
-            socket.send('wait')
-          }
-        })
-      })
-      socket.addEventListener('close', function () {
-        rs.emit('close')
-        rs.emit('end')
-        writable.end()
+  var writables = []
+
+  var send = function (data, from) {
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(data)
+    }
+  }
+  window.send = send
+
+  var onOpen = function () {
+    if (writables.length) {
+      send('continue')
+    }
+    socket.addEventListener('close', onClose)
+  }
+
+  var onMessage = function (data) {
+    blobToBuffer(data.data, function (err, buffer) {
+      if (err) return console.log(err)
+      writables.forEach(function (w) {
+        w.write(buffer) || send('wait')
       })
     })
-    writable.on('drain', function () {
-      socket.send('continue')
+  }
+  var onClose = function () {
+    writableOnClose()
+
+    socket.removeEventListener('close', onClose)
+    socket.removeEventListener('open', onOpen)
+    rs.emit('close')
+    rs.emit('end')
+    writables.forEach(function (w) {
+      w.end()
     })
+  }
+
+  var writableOnDrain = function () {
+    send('continue')
+  }
+  var writableOnClose = function (e) {
+    socket.removeEventListener('message', onMessage)
+    writables.forEach(function (w) {
+      w.removeListener('drain', writableOnDrain)
+      w.removeListener('close', writableOnClose)
+    })
+    send('wait')
+    writables = []
+  }
+
+  rs.pipe = function (w) {
+    writables.push(w)
+    w.on('drain', writableOnDrain)
+    w.on('close', writableOnClose)
+    socket.addEventListener('message', onMessage)
+    send('continue')
   }
   rs.destroy = function () {
     socket.close()
   }
+
+  socket.addEventListener('open', onOpen)
+
   return rs
 }
 
 var wsvideo = function (path, element) {
-  return new Videostream(new WsFile(path), element)
+  var file = new WsFile(path)
+  Videostream(file, element)
+  return function () {
+    file.socket.close()
+  }
 }
 
 window.wsvideo = wsvideo
